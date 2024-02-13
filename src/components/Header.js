e30e2843248913d8dbec130c96f019ee00e4e211
adesha <adeshaholagodu@gmail.com>
2024-02-13 19:08:56 +0530

    removed hard coded values

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,32 @@
-import { signOut } from 'firebase/auth'
-import React from 'react'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import React, { useEffect } from 'react'
 import { auth } from '../utils/firebase'
 import { useNavigate } from 'react-router-dom'
-
+import { useDispatch } from 'react-redux'
+import {addUser, removeUser} from '../utils/userSlice'
+import { LOGO, USER_ICON } from '../utils/constants'
 const Header = () => {
   const navigate=useNavigate()
-  
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    const unsubscribe=onAuthStateChanged(auth,(user)=>{
+      if(user){
+        const {uid,email,displayName}=user
+        dispatch(addUser({uid:uid,email:email,displayName:displayName}))
+        navigate('/browse')
+      }
+      else{
+        dispatch(removeUser())
+        navigate('/')
+      }
+    })
+
+    return ()=>unsubscribe()
+  },[])
+
   const handleSignOut=()=>{
     signOut(auth).then(()=>{
-      navigate('/')
     }).catch((err=>{
       navigate('/error')
     }))
@@ -16,14 +34,12 @@ const Header = () => {
 
   return (
     <div className='w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
-      <img className='w-44' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png
-' alt='logo'/>
+      <img className='w-44' src={LOGO} alt='logo'/>
     {auth.currentUser && <div className='flex p-2 m-2'>
       <div className='font-bold text-xl m-2'>{auth.currentUser.displayName}</div>
        <img
        className='w-12 h-12'
-       src='https://occ-0-4994-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229
-' alt='userIcon'
+       src={USER_ICON} alt='userIcon'
        />
        <button onClick={handleSignOut} className='m-2 font-bold text-white'>Sign Out</button>
     </div>}
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,11 +3,10 @@ import Header from './Header'
 import { checkValidateData } from '../utils/validate'
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile} from 'firebase/auth'
 import { auth } from '../utils/firebase'
-import { useNavigate } from 'react-router-dom'
+import { BACKGROUND_IMG } from '../utils/constants'
 
 const Login = () => {
 
-  const navigate=useNavigate()
   const [isSignInForm,setIsSignInForm]=useState(true)
   const [errorMessage,setErrorMessage]=useState(null)
 
@@ -35,7 +34,6 @@ const Login = () => {
         updateProfile(user,{
           displayName: name.current.value
         }).then(()=>{
-          navigate('/browse')
         }).catch((err)=>{
           setErrorMessage(err.message)
         })
@@ -50,7 +48,6 @@ const Login = () => {
       signInWithEmailAndPassword(auth,email.current.value,password.current.value)
       .then((userCredential)=>{
         const user=userCredential.user
-        navigate('/browse')
       })
       .catch((error)=>{
         const errorCode=error.code;
@@ -65,8 +62,7 @@ const Login = () => {
     <div >
       <Header/>
       <div className='absolute'>
-      <img src='https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_large.jpg
-' alt='logo'/>
+      <img src={BACKGROUND_IMG} alt='logo'/>
       </div>
       <form onSubmit={(e)=>e.preventDefault()} className='w-3/12 text-white absolute p-12 bg-black my-36 mx-auto right-0 left-0 bg-opacity-80'>
         <h1 className='font-bold text-3xl py-4'>{isSignInForm?'Sign In':'Sign Up'}</h1>
