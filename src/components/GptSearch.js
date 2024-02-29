c544b690b1f478ae63b91e1397f1b8ff29a800a8
adesha <adeshaholagodu@gmail.com>
2024-02-29 15:38:41 +0530

    added gpt search

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import GptSearchBar from './GptSearchBar'
+import GptMovieSuggestion from './GptMovieSuggestion'
+import { BACKGROUND_IMG } from '../utils/constants'
 
 const GptSearch = () => {
   return (
     <div>
-     
+    <div className='absolute -z-10'>
+      <img src={BACKGROUND_IMG} alt='logo'/>
+      </div>
+     <GptSearchBar/>
+     <GptMovieSuggestion/>
     </div>
   )
 }
