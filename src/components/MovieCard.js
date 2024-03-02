27ee1056d864971bf7669e6204a8f4ebc39abff6
adesha <adeshaholagodu@gmail.com>
2024-03-02 20:02:40 +0530

    showing gpt movie suggestion

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { IMAGE_URL } from '../utils/constants'
 
 const MovieCard = ({imagePath}) => {
+  if(!imagePath) return null
   return (
     <div className='w-48 pr-4'>
       <img src={IMAGE_URL+imagePath} alt='Movie name'/>
